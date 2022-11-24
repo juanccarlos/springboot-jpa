67bfc76bb33acdea1ceffa57aae47bf0302425b6
juanccarlos <juannnerysantos@gmail.com>
2022-11-24 07:46:38 -0300

    OrderStatus enum

--- src/main/java/com/juanprimeiroprojeto/curso/config/TesteConfig.java
+++ src/main/java/com/juanprimeiroprojeto/curso/config/TesteConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.juanprimeiroprojeto.curso.entites.Order;
 import com.juanprimeiroprojeto.curso.entites.User;
+import com.juanprimeiroprojeto.curso.entites.enums.OrderStatus;
 import com.juanprimeiroprojeto.curso.repositories.OrderRepository;
 import com.juanprimeiroprojeto.curso.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TesteConfig implements CommandLineRunner{
 		User u1 = new User(null, "Juan Carlos","juancarlos@gmail.com", "987337905", "123456");
 		User u2 = new User(null, "Evellyn Pereira","veve@gmail.com", "988883614", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WATTING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WATTING_PAYMENT, u1);
 		
 		
 		UserRepository.saveAll(Arrays.asList(u1,u2));
