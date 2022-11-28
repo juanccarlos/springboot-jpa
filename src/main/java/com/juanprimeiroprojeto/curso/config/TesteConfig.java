f8b25f3f94f2e4dbd14ae1f20f4b2d9928d3c57e
juanccarlos <juannnerysantos@gmail.com>
2022-11-28 04:25:33 -0300

    Payment, one-to-one association

--- src/main/java/com/juanprimeiroprojeto/curso/config/TesteConfig.java
+++ src/main/java/com/juanprimeiroprojeto/curso/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.juanprimeiroprojeto.curso.entites.Category;
 import com.juanprimeiroprojeto.curso.entites.Order;
 import com.juanprimeiroprojeto.curso.entites.OrderItem;
+import com.juanprimeiroprojeto.curso.entites.Payment;
 import com.juanprimeiroprojeto.curso.entites.Product;
 import com.juanprimeiroprojeto.curso.entites.User;
 import com.juanprimeiroprojeto.curso.entites.enums.OrderStatus;
@@ -87,6 +88,11 @@ public class TesteConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1 , oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1 );
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
